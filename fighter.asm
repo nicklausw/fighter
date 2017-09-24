ef39d312beb44075934e286246903b8d28664a83
nicklausw <nick@berlin.com>
2017-09-24 08:11:52 -0500

    stylistics change: ><^ instead of .b.w.l

--- fighter.asm
+++ fighter.asm
@@ -14,34 +14,34 @@ function Main {
 
   a16()
     // 32x32 sprites
-    lda.w #OBSIZE_32_64
-    sta.w OBSEL
+    lda #>OBSIZE_32_64
+    sta >OBSEL
 
     // random number, don't feel like dealing with this
-    //lda.w #$02ff
+    //lda #>$02ff
     //sta oam.reserved
 
     // $50 X and Y
-    lda.w #$50
-    sta.w oam.oamTable+0
-    sta.w oam.oamTable+1
+    lda #>$50
+    sta >oam.oamTable+0
+    sta >oam.oamTable+1
 
     // starting from sprite $30
-    lda.w #$30
-    sta.w oam.oamTable+2
-    stz.w oam.oamTable+3
+    lda #>$30
+    sta >oam.oamTable+2
+    stz >oam.oamTable+3
 
   a8()
 
     lda #%00010000  // enable sprites and plane 0
-    sta.w BLENDMAIN
+    sta >BLENDMAIN
 
     lda #$0f
-    sta.w PPUBRIGHT
+    sta >PPUBRIGHT
 
     // we want nmi
-    lda.b #VBLANK_NMI|AUTOREAD
-    sta.w PPUNMI
+    lda #<VBLANK_NMI|AUTOREAD
+    sta >PPUNMI
 
 
     cli // enable interrupts
@@ -49,37 +49,37 @@ function Main {
   forever:
   a8()
 
-    lda.w JOY1CUR+1
-    and.b #KEY_RIGHT >> 8
+    lda >JOY1CUR+1
+    and #<KEY_RIGHT >> 8
     beq +
 
-    lda.w oam.oamTable+0
+    lda >oam.oamTable+0
     clc; adc #$03
-    sta.w oam.oamTable+0
+    sta >oam.oamTable+0
 
-  +;lda.w JOY1CUR+1
-    and.b #KEY_LEFT >> 8
+  +;lda >JOY1CUR+1
+    and #<KEY_LEFT >> 8
     beq +
 
-    lda.w oam.oamTable+0
+    lda >oam.oamTable+0
     sec; sbc #$03
-    sta.w oam.oamTable+0
+    sta >oam.oamTable+0
 
-  +;lda.w JOY1CUR+1
-    and.b #KEY_UP >> 8
+  +;lda >JOY1CUR+1
+    and #<KEY_UP >> 8
     beq +
 
-    lda.w oam.oamTable+1
+    lda >oam.oamTable+1
     sec; sbc #$03
-    sta.w oam.oamTable+1
+    sta >oam.oamTable+1
 
-  +;lda.w JOY1CUR+1
-    and.b #KEY_DOWN >> 8
+  +;lda >JOY1CUR+1
+    and #<KEY_DOWN >> 8
     beq +
 
-    lda.w oam.oamTable+1
+    lda >oam.oamTable+1
     clc; adc #$03
-    sta.w oam.oamTable+1
+    sta >oam.oamTable+1
 
   +;
     
